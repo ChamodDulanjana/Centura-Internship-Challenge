12689f89f90bb0c48d9b3ade476457742023bda9
ChamodDulanjana <chamodperera128@gmail.com>
2024-08-25 09:23:51 +0530

    Create User component UI developed

---
+++ src/component/CreateUser.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const CreateUser = () => {
+    return (
+        <div className="flex justify-center items-center w-full flex-col">
+            <h1 className="font-bold text-xl mt-10 mb-10">Create User</h1>
+            <form
+                onSubmit={(e) => e.preventDefault()}
+
+            >
+                <input type="text" placeholder="UserId"
+                       className="border border-gray-200 rounded-md w-full px-2 py-1 mb-5"/>
+                <input type="text" placeholder="Username"
+                       className="border border-gray-200 rounded-md w-full px-2 py-1 mb-5"/>
+                <input type="email" placeholder="email"
+                       className="border border-gray-200 rounded-md w-full px-2 py-1 mb-5"/>
+                <input type="text" placeholder="phone"
+                       className="border border-gray-200 rounded-md w-full px-2 py-1 mb-5"/>
+                <button type="submit" className="border border-gray-200 rounded-md w-full px-2 py-1 mb-5 bg-green-400">Create User</button>
+            </form>
+        </div>
+    );
+};
+
+export default CreateUser;
