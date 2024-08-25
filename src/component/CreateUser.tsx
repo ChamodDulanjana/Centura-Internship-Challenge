4cf5f198852d3edab6e3af893506a5b08e42b185
ChamodDulanjana <chamodperera128@gmail.com>
2024-08-25 10:21:20 +0530

    commited!

--- src/component/CreateUser.tsx
+++ src/component/CreateUser.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, {FormEvent, useState} from 'react';
 import axios from 'axios';
 
 const CreateUser = () => {
+    const [uId, setUId] = useState("");
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
 
-    const handleSubmit = (e: React.FormEvent<HTMLButtonElement>) => {
+    const handleSubmit = async (e: FormEvent) => {
         e.preventDefault();
 
-
+        let userData = {
+            uId : uId,
+            name: name,
+            email: email,
+            phone: phone,
+        }
     }
 
 
@@ -18,13 +27,13 @@ const CreateUser = () => {
                 onSubmit={(e) => e.preventDefault()}
 
             >
-                <input type="text" placeholder="UserId"
+                <input type="text" placeholder="UserId" value={uId} onChange={(e) => setUId(e.target.value)} />
                        className="border border-gray-200 rounded-md w-full px-2 py-1 mb-5"/>
-                <input type="text" placeholder="Username"
+                <input type="text" placeholder="Username" value={name} onChange={(e) => setName(e.target.value)} />
                        className="border border-gray-200 rounded-md w-full px-2 py-1 mb-5"/>
-                <input type="email" placeholder="email"
+                <input type="email" placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)} />
                        className="border border-gray-200 rounded-md w-full px-2 py-1 mb-5"/>
-                <input type="text" placeholder="phone"
+                <input type="text" placeholder="phone" value={phone} onChange={(e) => setPhone(e.target.value)} />
                        className="border border-gray-200 rounded-md w-full px-2 py-1 mb-5"/>
                 <button type="submit"
                         className="border border-gray-200 rounded-md w-full px-2 py-1 mb-5 bg-green-400"
