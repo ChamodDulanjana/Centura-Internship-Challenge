c45640a41a343a7faa672a8e5e15bccf49ffd0d4
ChamodDulanjana <chamodperera128@gmail.com>
2024-08-25 09:46:06 +0530

    Install Axios

--- src/component/CreateUser.tsx
+++ src/component/CreateUser.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
+import axios from 'axios';
 
 const CreateUser = () => {
+
+    const handleSubmit = (e: React.FormEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+
+
+    }
+
+
+
     return (
         <div className="flex justify-center items-center w-full flex-col">
             <h1 className="font-bold text-xl mt-10 mb-10">Create User</h1>
@@ -16,7 +26,10 @@ const CreateUser = () => {
                        className="border border-gray-200 rounded-md w-full px-2 py-1 mb-5"/>
                 <input type="text" placeholder="phone"
                        className="border border-gray-200 rounded-md w-full px-2 py-1 mb-5"/>
-                <button type="submit" className="border border-gray-200 rounded-md w-full px-2 py-1 mb-5 bg-green-400">Create User</button>
+                <button type="submit"
+                        className="border border-gray-200 rounded-md w-full px-2 py-1 mb-5 bg-green-400"
+                        onClick={(e) => handleSubmit(e)}
+                >Create User</button>
             </form>
         </div>
     );
